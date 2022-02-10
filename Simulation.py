896e75898f29bea3d0897f9ee2f9804b48b5de5b
Jackson Rusch <jackson.p.rusch@vanderbilt.edu>
2022-02-10 17:31:29 -0600

    initial commit

---
+++ Simulation.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+p_robot = [0,0]
+
+while True:
+    # Initial graph formulation
+    print(p_robot)
+    
+while True:
+    # Get distance of the robot
+
+    # Update occupancy grid
+
+    # Update weights in the graph
+
+    # Generate refinement points into the graph
+
+    # Get new path for the robot
+    print(p_robot)
