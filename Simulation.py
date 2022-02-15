9f6cbe310b3273a2d27449d861ef1f62af2e4721
Jackson Rusch <jackson.p.rusch@vanderbilt.edu>
2022-02-15 12:06:57 -0600

    added very poor code for initial graph generation

--- Simulation.py
+++ Simulation.py
@@ -1,14 +1,13 @@
 import numpy as np
 from graph import Graph, Edge, Node, Point
+import time
 
 class Simulation():
     def __init__(self, p_start, p_goal, obstacles):
         # Parameters
-        self.max_iterations = 1000 # max iterations (number of points) to attempt initial graph creation
+        self.max_iterations = 100 # max iterations (number of points) to attempt initial graph creation
         self.sensor_theta = 0.78 # the sensing arc angle in radians
         self.sensor_max_distance = 3 #the sensing distance
-        self.goal_radius = 1
-        self.connection_radius = 2
 
         self.p_goal = p_goal
         self.p_robot = p_start
@@ -17,21 +16,29 @@ class Simulation():
         self.occupancy_grid = np.full((30,30,2), False) 
         self.graph = Graph()
         
-        self.goal_node = Node(self.p_goal.x, self.p_goal.y)
+        self.goal_node = Node(Point(self.p_goal.x, self.p_goal.y))
         self.graph.add_node(self.goal_node)
         
         self.path = None
 
     # Returns T/F whether or not an initial graph was found
     def graph_creation(self):
-        self.start_node = Node(self.p_robot.x, self.p_robot.y)
+        self.start_node = Node(Point(self.p_robot.x, self.p_robot.y))
         self.graph.add_node(self.start_node)
+        
+        self.path = self.graph.find_shortest_path(self.start_node, self.goal_node)
+        
         # Initial graph formulation
         i = 0
         while self.path is None and i < self.max_iterations:
+            # create an exploratory point
+            node = self.graph.generate_exploratory_node(self.start_node, self.goal_node)
+            
+            # add the node to the graph
+            self.graph.add_node(node)
+            
             # check if a path reaches goal
             self.path = self.graph.find_shortest_path(self.start_node, self.goal_node)
-            
             i += 1
         
         # No path was found in our maximum allowed iterations
@@ -56,18 +63,19 @@ class Simulation():
         # Get new path for the robot
 
 
-p_start = Point(5,2,1.56)
-p_goal = Point(5,18)
+p_start = Point(5.0,2.0,1.56)
+p_goal = Point(5.0,18.0)
 obstacles = [Point(30,30), Point(15.8,19.3)] # list of points
 sim = Simulation(p_start, p_goal, obstacles)
 
 sim.graph_creation()
-
+print(sim.graph)
 # while the sim has a path and it hasn't finished the path
-while sim.path is not None and len(sim.path) != 0:
-    sim.step()
+# while sim.path is not None and len(sim.path.path) != 0:
+#     sim.step()
+#     time.sleep(1)
 
-    # if our path becomes invalid (obstacle blocked the path)
-    if sim.path is None:
-        # attempt to create a new graph
-        sim.graph_creation()
+#     # if our path becomes invalid (obstacle blocked the path)
+#     if sim.path is None:
+#         # attempt to create a new graph
+#         sim.graph_creation()
--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 # Holds x, y, and theta position. supports equality; set(x,y); set(point); distance(point)
 class Point():        
@@ -51,24 +52,96 @@ class Path():
 
 class Graph():
     def __init__(self):
+        self.goal_radius = 1
+        self.connection_radius = 2
         self.graph = []
 
     def add_node(self, node):
         self.graph.append(node)
         node.index = len(self.graph) - 1
+        
+        for n in self.graph:
+            if node.distance(n) < self.connection_radius and node != n:
+                # connect with an edge
+                node.add_edge(n)
     
     def get_node(self, index):
         return self.graph[index]
 
-    def find_shortest_path(self, starting_node, goal_node):
+    def generate_exploratory_node(self, start_node, goal_node):
+        # consider idea of how many bits do you need to know where you are in the environemnt based on 
+        # where the points are (aka, if you only have 2 points, you wouldnt know where you were in all the empty areas because
+        # you have nothing to go off of)
+        # node_list, visited = self.dijkstras(starting_node, goal_node, True)
+        
+        # order nodes by distance
+        distances = [[node.index, 1 / (len(node.edges) + 1)] for node in self.graph]
+        #sorted(distances, key=lambda x: x[1], reverse=True) # sort from 
+        distances[goal_node.index][1] = 0
+        # weighted choice with node
+        weights = [x[1] for x in distances]
+        node = self.graph[random.choices(distances, weights=tuple(weights), k=1)[0][0]]
+        
+        x,y = self.new_point_direction(node, goal_node)
+        mag = random.random() * self.connection_radius / 2 + self.connection_radius / 2
+        return Node(Point(node.pos.x + x * mag, node.pos.y + y * mag, node.pos.theta + random.random() / 2 - 0.25))
+      
+    # gross clean up later  
+    def new_point_direction(self, node, goal_node):
+        x = 0.0
+        y = 0.0
+        print(node)
+        # force away from other points
+        for n in self.graph:
+            x_dir = node.pos.x - n.pos.x
+            y_dir = node.pos.y - n.pos.y
+            mag = math.sqrt(x_dir ** 2 + y_dir ** 2)
+            if mag == 0:
+                continue
+            
+            x_dir /= mag
+            y_dir /= mag
+            
+            force = 1 / node.distance(n) ** 2
+            
+            x += force * x_dir
+            y += force * y_dir
+        
+        # force towards goal
+        x_dir = goal_node.pos.x - node.pos.x
+        y_dir = goal_node.pos.y - node.pos.y
+        print(x_dir, y_dir)
+        mag = math.sqrt(x_dir ** 2 + y_dir ** 2)
+        if mag != 0:
+            x_dir /= mag
+            y_dir /= mag
+            
+            force = node.distance(goal_node) ** 2
+            
+            x += force * x_dir
+            y += force * y_dir
+            
+        mag = math.sqrt(x ** 2 + y ** 2)
+        if mag == 0:
+            x += 1
+        else:
+            x /= mag
+            y /= mag
+        
+        return x,y
+        
+    # gross clean up later
+    # if a goal node is denoted, it will run the modified a_star encoding
+    def dijkstras(self, starting_node, goal_node):
         starting_node_index = starting_node.index
-        goal_node_index = goal_node.index
         num_nodes = len(self.graph)
         node_list = [[None, None] for i in range(num_nodes)] #current weight number, and what parent it has in the tree. Both initialize to None
         node_list[starting_node_index][0] = 0
         visited = [False] * num_nodes
         
-        while not visited[goal_node_index]:           
+        # if we are in a_star mode, continue until all possible nodes are searched. Otherwise, end when
+        # goal is reached
+        while not visited[goal_node.index]:           
             # get the smallest node to connect in
             smallest_pos = -1
             for i in range(len(node_list)):
@@ -90,10 +163,15 @@ class Graph():
                 
             visited[smallest_pos] = True
         
+        return node_list, visited
+        
+    # given a starting node and goal node of the graph, return a Path (if one exists)
+    def find_shortest_path(self, starting_node, goal_node):
+        node_list, visited = self.dijkstras(starting_node, goal_node)
+        goal_node_index = goal_node.index
+        
         if not visited[goal_node_index]:
             return None
-        
-        
         
         path = Path()
         n_index = goal_node_index
@@ -101,8 +179,14 @@ class Graph():
             path.prepend_node(self.graph[n_index])
             n_index = node_list[n_index][1]
             
-        return path
-            
+        return path    
+    
+    def __str__(self):
+        s = ''
+        for n in self.graph:
+            s += str(n) + '\n'
+        
+        return s
             
 class Node():
     def __init__(self, point):
@@ -111,12 +195,16 @@ class Node():
         index = -1
         
     def __str__(self):
-        return 'Node ' + str(self.index) + ': ' + str(self.pos)
+        s = 'Node ' + str(self.index) + ': ' + str(self.pos) + ' | Edges: '
+        for e in self.edges:
+            s += ' ' + str(e)
+            
+        return s
         
     def distance(self, node2):
         return self.pos.distance(node2.pos)
 
-    def add_edge(self, node2):
+    def add_edge(self, node2): # TODO check for obstacle
         self.edges.append(Edge(self.index, node2.index, self.distance(node2), True))
         node2.edges.append(Edge(node2.index, self.index, self.distance(node2), True))
 
@@ -133,26 +221,29 @@ class Edge():
         self.weight = distance
         self.active = active
         
+    def __str__(self):
+        return '(' + str(self.node_2_index) + ',' + str(self.weight) + ',' + str(self.active) + ')'
+        
         
 # testing path creation
-g = Graph()
-g.add_node(Node(Point(0,0)))
-g.add_node(Node(Point(0,1)))
-g.add_node(Node(Point(0,2)))
-g.add_node(Node(Point(1,0)))
-g.add_node(Node(Point(1,1)))
-g.add_node(Node(Point(1,2)))
-g.add_node(Node(Point(2,0)))
-g.add_node(Node(Point(2,1)))
-g.add_node(Node(Point(2,2)))
-g.get_node(0).add_edge(g.get_node(1))
-g.get_node(0).add_edge(g.get_node(8))
-g.get_node(4).add_edge(g.get_node(5))
-g.get_node(4).add_edge(g.get_node(6))
-g.get_node(2).add_edge(g.get_node(7))
-g.get_node(3).add_edge(g.get_node(7))
-g.get_node(1).add_edge(g.get_node(5))
-g.get_node(8).add_edge(g.get_node(6))
+# g = Graph()
+# g.add_node(Node(Point(0,0)))
+# g.add_node(Node(Point(0,1)))
+# g.add_node(Node(Point(0,2)))
+# g.add_node(Node(Point(1,0)))
+# g.add_node(Node(Point(1,1)))
+# g.add_node(Node(Point(1,2)))
+# g.add_node(Node(Point(2,0)))
+# g.add_node(Node(Point(2,1)))
+# g.add_node(Node(Point(2,2)))
+# g.get_node(0).add_edge(g.get_node(1))
+# g.get_node(0).add_edge(g.get_node(8))
+# g.get_node(4).add_edge(g.get_node(5))
+# g.get_node(4).add_edge(g.get_node(6))
+# g.get_node(2).add_edge(g.get_node(7))
+# g.get_node(3).add_edge(g.get_node(7))
+# g.get_node(1).add_edge(g.get_node(5))
+# g.get_node(8).add_edge(g.get_node(6))
 
-path = g.find_shortest_path(g.get_node(0), g.get_node(6))
-print(path)
+# path = g.find_shortest_path(g.get_node(0), g.get_node(6))
+# print(path)
