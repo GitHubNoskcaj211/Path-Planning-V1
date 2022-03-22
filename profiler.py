96d957212246481bc5ed415d571394c936e12ae8
Jackson Rusch <jackson.p.rusch@vanderbilt.edu>
2022-03-22 11:35:51 -0500

    profiling

--- profiler.py
+++ profiler.py
@@ -4,22 +4,30 @@ import random
 from tree import Tree
 from occupancygrid import OccupancyGrid
 
-start = (2.5,2.5,0)
-goal = (25.5,28.5)
-# xmin, ymin, xmax, ymax
-world_bounds = (0,0,32,32)
 
-occupancy = OccupancyGrid(world_bounds, 6, math.pi / 4)
-for i in range(20):
-    x_pos = math.floor(random.random() * 32)
-    y_pos = math.floor(random.random() * 32)
-    occupancy.grid[y_pos, x_pos, 0] = True
-
-rrtree = Tree((start[0], start[1]), 3, world_bounds, occupancy)
 # rrtree.build_tree(4000)
 
-cProfile.run('rrtree.build_tree(1500)')
+def run():
+    start = (2.5,2.5,0)
+    goal = (25.5,28.5)
+    # xmin, ymin, xmax, ymax
+    world_bounds = (0,0,32,32)
 
+    occupancy = OccupancyGrid(world_bounds, 6, math.pi / 4)
+    for i in range(30):
+        x_pos = math.floor(random.random() * 32)
+        y_pos = math.floor(random.random() * 32)
+        occupancy.grid[y_pos, x_pos, 0] = True
+
+    rrtree = Tree((start[0], start[1]), 3, world_bounds, occupancy)
+    rrtree.build_tree(2000)
+    rrtree.clear_tree()
+
+def time():
+    for i in range(2):
+        run()
+        
+cProfile.run('time()')
 
 # new_node = rrtree.generate_random_node()
 # while rrtree.node_in_obstacle(new_node):
